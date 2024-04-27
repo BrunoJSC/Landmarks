f8a80b8b56bc60f9fcb7a305e188a402c0a01209
Bruno de Jesus <bruno.jesus.carmo@gmail.com>
2024-04-27 02:23:25 -0300

    first commit

---
+++ Landmarks/PageView/PageView.swift
@@ -0,0 +1,27 @@
+//
+//  PageView.swift
+//  Landmarks
+//
+//  Created by bruno on 27/04/24.
+//
+
+import SwiftUI
+
+
+struct PageView<Page: View>: View {
+    var pages: [Page]
+    @State private var currentPage = 1
+    
+    var body: some View {
+        VStack {
+            PageViewController(pages: pages, currentPage: $currentPage)
+            Text("Current Page: \(currentPage)")
+        }
+        .aspectRatio(3 / 2, contentMode: .fit)
+    }
+}
+
+
+#Preview {
+    PageView(pages: ModelData().features.map { FeatureCard(landmark: $0) })
+}
